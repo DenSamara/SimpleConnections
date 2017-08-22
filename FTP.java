91986fc5e8bce0bf2ee818408e643d8507c6f7a2
Denis Konovalov <densamara@yandex.ru>
2017-08-22 15:52:43 +0400

    Add files via upload

---
+++ FTP.java
@@ -0,0 +1,376 @@
+package com.dalimo.mtrade.connectivity;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import android.util.Log;
+
+public class FTP {
+	public final static String WIN1251 = "windows-1251";
+
+	private final static short bufSize = 1024;
+
+	private final static String TAG = "!->FTP";
+
+	// private final static short LOGIN_SUCCESS = 334;
+
+	public final static String BINARY_FILE_TYPE = "TYPE I";
+	public final static String ASCII_FILE_TYPE = "TYPE A";
+
+	private Socket _socket = null;
+
+	private OutputStream _out = null;
+	private InputStream _in = null;
+
+	private Socket dataSocket = null;
+	private InputStream data = null;
+	// private OutputStream dataOut = null;
+
+	public final static byte DEFAULT_PORT = 21;
+	public byte mTimeout = 10;
+
+	private String mServer;
+	private String mLogin;
+	private String mPassword;
+	private byte mPort;
+	private boolean mIsConnected;
+
+	/*
+	 * TODO: Конструктор
+	 */
+	public FTP(String server, String login, String passw) {
+		mServer = server;
+		mLogin = login;
+		mPassword = passw;
+		mPort = DEFAULT_PORT;
+		mIsConnected = false;
+	}
+
+	/**
+	 * TODO: Конструктор со значениями по-умолчанию
+	 */
+	public FTP() {
+		this("ftp.microsoft.com", "anonymous", "ftp.microsoft.com");
+	}
+
+	// TODO: send
+	private void send(String message) {
+		if (!mIsConnected)
+			return;
+
+		try {
+			_out.write(message.getBytes());
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+	}
+
+	// TODO: sendWin1251
+	private void sendWin1251(String message) {
+		if (!mIsConnected)
+			return;
+
+		try {
+			_out.write(message.getBytes(WIN1251));
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+	}
+
+	private String receive() {
+		try {
+			byte[] buf = new byte[bufSize];
+			_in.read(buf);
+			return new String(buf);
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			return "";
+		}
+	}
+
+	public boolean connect(String server, String login, String password,
+			byte port) {
+		if (server.length() == 0) {
+			Log.e(TAG, "Имя сервера не может быть пустым");
+			return false;
+		}
+
+		mServer = server;
+		mLogin = login;
+		mPassword = password;
+		if (port != 0 && port != DEFAULT_PORT && port > 0) {
+			mPort = port;
+		}
+
+		try {
+			InetSocketAddress sockaddr = new InetSocketAddress(server, port);
+
+			_socket = new Socket();
+
+			_socket.connect(sockaddr, mTimeout * 1000);
+
+			_in = _socket.getInputStream();
+
+			_out = _socket.getOutputStream();
+
+			String unswer = receive();
+
+			// Log.i("!->FTPConnect", unswer);
+
+			if (unswer.contains("220")) {
+				Log.i(TAG, "Connect success");
+				mIsConnected = true;
+			} else {
+				Log.i(TAG, "Connect fail");
+				mIsConnected = false;
+			}
+		} catch (Exception e) {
+			Log.e(TAG, e.toString() + " to: " + server);
+			mIsConnected = false;
+		}
+
+		return mIsConnected;
+	}
+
+	// TODO: connect()
+	public boolean connect() {
+		return connect(mServer, mLogin, mPassword, mPort);
+	}
+
+	// TODO: login
+	public boolean login(String login, String password) {
+		if (login.length() == 0) {
+			Log.e(TAG, "Имя пользователя не может быть пустым");
+			return false;
+		}
+
+		if (!mIsConnected) {
+			Log.e(TAG, "Сначала нужно выполнить подключение");
+			return false;
+		}
+
+		send(String.format("USER %s\r\n", login));
+		String unswer = receive();
+		// Log.i("!->USER", unswer);
+
+		send(String.format("PASS %s\r\n", password));
+		unswer = receive();
+		// Log.i("!->PASS", unswer);
+
+		if (unswer.contains("230")) {
+			Log.i(TAG, "Login success");
+			return true;
+		} else {
+			Log.i(TAG, "Login failed");
+			return false;
+		}
+	}
+
+	// TODO: login()
+	public boolean login() {
+		return login(mLogin, mPassword);
+	}
+
+	// TODO: openDataConnection
+	private boolean openDataConnection(InetSocketAddress sockaddr) {
+		if (!mIsConnected) {
+			return false;
+		}
+
+		try {
+			dataSocket = new Socket();
+
+			dataSocket.connect(sockaddr, mTimeout * 1000);
+
+			data = dataSocket.getInputStream();
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			return false;
+		}
+
+		Log.i(TAG, "Data connection successfull");
+		return true;
+	}
+
+	// TODO: closeDataConnection
+	private void closeDataConnection() {
+		if (!mIsConnected)
+			return;
+
+		try {
+			if (data != null)
+				data.close();
+			if (dataSocket != null)
+				dataSocket.close();
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+	}
+
+	// TODO: parseDataConnectionString
+	private InetSocketAddress parseDataConnectionString(String serverString) {
+		InetSocketAddress result = null;
+		// String server = "";
+		int port = 0;
+
+		int posFirst = serverString.indexOf("(");
+		int posLast = serverString.indexOf(")");
+
+		String addr = serverString.substring(posFirst + 1, posLast);
+
+		String[] bytes = addr.split(",");
+
+		try {
+			port = Integer.parseInt(bytes[4]) * 256
+					+ Integer.parseInt(bytes[5]);
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+
+		result = new InetSocketAddress(mServer, port);
+		return result;
+	}
+
+	// TODO: enterLocalPassiveMode
+	private boolean enterLocalPassiveMode() {
+		send(String.format("PASV\r\n"));
+		String unswer = receive();
+
+		if (unswer.contains("227")) {
+			if (!openDataConnection(parseDataConnectionString(unswer))) {
+				Log.e(TAG, "Data connection fail");
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	// TODO: setFileTransferMode
+	public void setFileTransferMode(String mode) {
+		if (!mIsConnected) {
+			Log.e(TAG, "Change FileTransferMode fail. Connect before");
+		}
+
+		send(String.format("TYPE %s\r\n", mode));
+		receive();// String unswer = receive();
+		Log.i(TAG, "FileTransferMode successfull changed");
+	}
+
+	// TODO: changeWorkingDirectory
+	@SuppressWarnings("deprecation")
+	public boolean changeWorkingDirectory(String dirName) {
+		if (!mIsConnected) {
+			Log.e(TAG, "ChangeWorkingDirectory fail. Connect before");
+			return false;
+		}
+
+		sendWin1251(String.format("CWD %s\r\n", dirName));
+		String unswer = "";
+		try {
+			unswer = new String(receive().getBytes(WIN1251), 0);
+		} catch (Exception e) {
+
+		}
+		if (unswer.contains("250")) {
+			Log.i(TAG, "Directory changed");
+			return true;
+		} else {
+			Log.i(TAG, unswer);
+			return false;
+		}
+	}
+
+	// TODO: retrieveFile
+	public boolean retrieveFile(String name, OutputStream os) {
+		if (!mIsConnected)
+			return false;
+
+		if (!enterLocalPassiveMode()) {
+			closeDataConnection();
+			return false;
+		}
+
+		sendWin1251(String.format("RETR %s\r\n", name));
+		String unswer = receive();
+
+		Log.i(TAG, "Transfer starting");
+
+		if (unswer.contains("550")) {
+			Log.e(TAG, "Файл не существует/r/n" + unswer);
+			closeDataConnection();
+			return false;
+		}
+
+		try {
+			byte[] buf = new byte[bufSize];
+			int cnt = 0;
+			while ((cnt = data.read(buf)) != -1) {
+				os.write(buf, 0, cnt);
+			}
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			closeDataConnection();
+			return false;
+		}
+
+		unswer = receive();
+
+		if (!unswer.contains("226")) {
+			Log.e(TAG, unswer);
+			closeDataConnection();
+			return false;
+		}
+
+		closeDataConnection();
+		Log.i(TAG, "Transfer complete");
+		return true;
+	}
+
+	// TODO: logout
+	private void logout() {
+		if (!mIsConnected)
+			return;
+
+		send(String.format("QUIT\r\n"));
+		String unswer = receive();
+		if (unswer.contains("221")) {
+			Log.i(TAG, "Session closed");
+		} else {
+			Log.e(TAG, unswer);
+		}
+	}
+
+	// TODO: disconnect
+	public void disconnect() {
+		if (!mIsConnected)
+			return;
+
+		mIsConnected = false;
+
+		logout();
+
+		try {
+			if (_in != null)
+				_in.close();
+			if (_out != null)
+				_out.close();
+
+			if (_socket != null)
+				_socket.close();
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+	}
+
+	/**
+	 * Возвращает признак подключения
+	 * 
+	 * @return true если подключен, false - иначе
+	 */
+	public boolean isConnected() {
+		return mIsConnected;
+	}
+}
---
+++ SMTP.java
@@ -0,0 +1,367 @@
+package com.dalimo.mtrade.connectivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import android.util.Base64;
+import android.util.Log;
+
+import com.dalimo.mtrade.Const;
+
+public class SMTP {
+	private final static short bufSize = 1024;
+
+	private final static String TAG = "!->SMTP";
+	private final static String BOUND = "-------------------MTrade";
+	// private final static String CHARSET_NAME = "UTF-8";
+
+	private final static short CONNECT_SUCCESS = 220;
+	private final static short QUIT_SUCCESS = 221;
+	private final static short PASSWORD_SUCCESS = 235;
+	private final static short GENERIC_SUCCESS = 250;
+	private final static short LOGIN_SUCCESS = 334;
+	private final static short DATA_SUCCESS = 354;
+
+	private static Socket _socket = null;
+
+	private static OutputStream _out = null;
+	private static InputStream _in = null;
+
+	public static final byte DEFAULT_PORT = 25;
+	public static byte mTimeout = 60;
+
+	private static String mServer;
+	private static String mLogin;
+	private static String mPassword;
+
+	private static int mPort = DEFAULT_PORT;
+
+	private static String mLastError;
+
+	public static String getLastError() {
+		return mLastError;
+	}
+
+	public static void setServer(String server) {
+		mServer = server;
+	}
+
+	public static void setLogin(String login) {
+		mLogin = login;
+	}
+
+	public static void setPassword(String password) {
+		mPassword = password;
+	}
+
+	public static void setPort(int port) {
+		if (0 < port || port < 65535) {
+			mPort = port;
+		} else {
+			mPort = DEFAULT_PORT;
+		}
+	}
+
+	private static void send(String message) throws IOException {
+		send(message.getBytes());
+	}
+
+	private static void send(byte[] message) throws IOException {
+		_out.write(message);
+	}
+
+	private static void send1251(String message) {
+		try {
+			_out.write(message.getBytes(Const.WIN1251));
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+		}
+	}
+
+	private static void sendAndCodeToBase64(byte[] buffer, int mode)
+			throws IOException {
+		_out.write(Base64.encode(buffer, mode));
+	}
+
+	private static void sendAndCodeToBase64(String message) throws IOException {
+		sendAndCodeToBase64(message.getBytes(), Base64.DEFAULT);
+	}
+
+	/*
+	 * private static void sendAndCodeToBase64_1251(String message) throws
+	 * IOException { _out.write(Base64.encode(message.getBytes(App.WIN1251),
+	 * Base64.DEFAULT)); }
+	 */
+
+	private static String receive() throws IOException {
+		byte[] buf = new byte[bufSize];
+		_in.read(buf);
+		return new String(buf);
+	}
+
+	private static boolean connect() throws IOException {
+		if (mServer.length() == 0) {
+			mLastError = "Имя сервера не может быть пустым";
+			return false;
+		}
+
+		if (mLogin.length() == 0) {
+			mLastError = "Имя пользователя не может быть пустым";
+			return false;
+		}
+
+		try {
+			InetSocketAddress sockaddr = new InetSocketAddress(mServer, mPort);// _smtpAddress,
+																				// _port
+
+			_socket = new Socket();
+
+			_socket.connect(sockaddr, mTimeout * 1000);
+
+			_in = _socket.getInputStream();
+
+			_out = _socket.getOutputStream();
+		} catch (Exception e) {
+			mLastError = e.getMessage();
+			Log.e(TAG, e.getMessage() + mServer);
+			return false;
+		}
+
+		return true;
+	}
+
+	private static void disconnect() throws IOException {
+		if (_in != null)
+			_in.close();
+		if (_out != null)
+			_out.close();
+
+		_socket.close();
+	}
+
+	public static boolean sendMail(String codeAgent, EMailMessage mailData) throws IOException {
+		Log.i(TAG, "Try to connect to " + mServer);
+		if (!connect()) {
+			Log.e(TAG, "Connect fail");
+			disconnect();
+			return false;
+		}
+
+		if (_out == null || _in == null) {
+			Log.e(TAG, "Stream error");
+			return false;
+		}
+
+		String message = receive();
+
+		if (checkResponse(message) != CONNECT_SUCCESS) {
+			mLastError = "Connect fail\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		message = String.format("HELO %s\r\n", mServer);
+
+		send(message);
+
+		message = receive();
+
+		if (checkResponse(message) != GENERIC_SUCCESS) {
+			mLastError = "HELO fail\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		// Начинаем авторизовываться
+		send("AUTH LOGIN\r\n");
+		message = receive();
+
+		if (checkResponse(message) != LOGIN_SUCCESS) {
+			mLastError = "AUTH LOGIN fail\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		// шлем логин
+		Log.i(TAG, "Send login");
+		sendAndCodeToBase64(mLogin.getBytes(), Base64.CRLF);
+		message = receive();
+		if (checkResponse(message) != LOGIN_SUCCESS) {
+			mLastError = "Некорректное имя пользователя\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		// шлем пароль
+		Log.i(TAG, "Send password");
+		sendAndCodeToBase64(mPassword.getBytes(), Base64.CRLF);
+		message = receive();
+		if (checkResponse(message) != PASSWORD_SUCCESS) {
+			mLastError = "Неверный пароль\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		Log.i(TAG, "Authorization complete");
+
+		// От кого
+		message = String.format("MAIL FROM: %s\r\n", mailData.From());
+		send(message);
+		message = receive();
+		if (checkResponse(message) != GENERIC_SUCCESS) {
+			mLastError = "MAIL FROM fail\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		// Кому
+		message = String.format("RCPT TO: %s\r\n", mailData.To());
+		send(message);
+		message = receive();
+		if (checkResponse(message) != GENERIC_SUCCESS) {
+			mLastError = "RCPT TO fail" + message;
+			disconnect();
+			return false;
+		}
+
+		// Данные--------------------------------------------------------------------
+		Log.i(TAG, "Send data");
+		send("DATA\r\n");
+		message = receive();
+
+		if (checkResponse(message) != DATA_SUCCESS) {
+			mLastError = "Ошибка отправки секции DATA\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		// Формируем заголовок
+		String tmp = codeAgent;
+
+		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHHmmss",
+				Locale.ENGLISH);
+
+		String mesID = String.format("%s.%s%s", tmp, sdf.format(System
+				.currentTimeMillis()),
+				mailData.From().substring(mailData.From().lastIndexOf('@')));
+
+		StringBuilder header = new StringBuilder();
+
+		sdf.applyPattern("ddd, dd MMM yyyy HH:mm:ss");
+
+		header.append(String.format("Date: %s +0400\r\n",
+				sdf.format(System.currentTimeMillis())));
+		header.append(String.format("From: %s\r\n", mailData.From()));
+		header.append("X-Priority: 3 (Normal)\r\n");
+		header.append(String.format("Message-ID: <%s>\r\n", mesID));
+		header.append(String.format("To: %s\r\n", mailData.To()));
+		header.append(String.format("Subject: %s\r\n", mailData.Subject()));
+		header.append("MIME-Version: 1.0\r\n");
+
+		// Если вложений нет, отправляем
+		Log.i(TAG, "Send message");
+		if (mailData.getAttachments().size() == 0) {
+			header.append(String.format(
+					"Content-Type: text/plain; charset=%s\r\n", Const.UTF8));
+			header.append("Content-Transfer-Encoding: base64\r\n\r\n");
+
+			send(header.toString());
+			sendAndCodeToBase64(mailData.Body());
+		} else// вложения
+		{
+			// header = new StringBuilder();
+			header.append(String
+					.format("Content-Type: multipart/mixed;\r\n boundary=\"%s\"\r\n\r\n",
+							BOUND));
+
+			header.append("--" + BOUND + "\r\n");
+			header.append("Content-Type: text/plain; charset=Windows-1251\r\n");
+			header.append("Content-Transfer-Encoding: base64\r\n\r\n");
+
+			send(header.toString().getBytes(Const.WIN1251));
+
+			sendAndCodeToBase64(mailData.Body().getBytes(Const.WIN1251),
+					Base64.DEFAULT);
+
+			for (String item : mailData.getAttachments()) {
+				byte last = (byte) item.lastIndexOf(File.separator);
+				last += 1;
+				String strName = item.substring(last, item.length());
+
+				header = new StringBuilder();
+				header.append("--" + BOUND + "\r\n");
+				header.append(String.format(
+						"Content-Type: TEXT/PLAIN; name=\"%s\"\r\n", strName));
+				header.append("Content-transfer-encoding: base64\r\n");
+				header.append(String
+						.format("Content-Disposition: attachment; filename=\"%s\"\r\n\r\n",
+								strName));
+
+				send1251(header.toString());
+
+				// Далее - непосредственно файл
+				File att = new File(item);
+
+				if (att.canRead()) {
+					FileInputStream fis = new FileInputStream(att);
+
+					int size = att.length() >= Integer.MAX_VALUE ? Integer.MAX_VALUE
+							: (int) att.length();
+					byte[] buf = new byte[size];
+					fis.read(buf);
+					sendAndCodeToBase64(buf, Base64.CRLF);
+					fis.close();
+				}
+			}
+
+			send(String.format("--%s--\r\n", BOUND));
+		}
+
+		// Заканчиваем
+		send("\r\n.\r\n");
+		message = receive();
+		if (checkResponse(message) != GENERIC_SUCCESS) {
+			mLastError = "Ошибка отправки данных\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		// Завершаем сеанс
+		Log.i(TAG, "Quit");
+		send("QUIT\r\n");
+		message = receive();
+		if (checkResponse(message) != QUIT_SUCCESS) {
+			mLastError = "Ошибка при завершении сеанса\r\n" + message;
+			disconnect();
+			return false;
+		}
+
+		disconnect();
+
+		return true;
+	}
+
+	private static short checkResponse(String str) {
+		short res = 0;
+
+		if (str.length() < 3)
+			return res;
+
+		try {
+			res = Short.valueOf(str.substring(0, 3));
+		} catch (Exception e) {
+			mLastError = e.getMessage();
+			Log.e(TAG, e.getMessage());
+		}
+
+		return res;
+	}
+
+}
